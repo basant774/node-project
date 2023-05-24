e2da4abb57ade025837c26159b7afd140c3f90f6
ahmeddapash22 <117464794+ahmeddapash22@users.noreply.github.com>
2023-05-25 00:45:42 +0300

    Revert "Add files via upload"

--- auth.guard.js
+++
@@ -1,9 +0,0 @@
-exports.isAuth = (req, res, next) => {
-  if (req.session.userId) next();
-  else res.redirect("/login");
-};
-
-exports.isNotAuth = (req, res, next) => {
-  if (!req.session.userId) next();
-  else res.redirect("/");
-};
